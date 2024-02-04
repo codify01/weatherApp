1761b27230111b19968426092e8c8c16703e1a4e
Uthman Oluwaseun <amoleuthman@gmail.com>
2024-02-04 18:35:09 +0100

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-let endpoint = "https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=705303948810324980db093f63a51039&units=metric"
 const fetchMe = () => {
+    let userCity = document.getElementById('searchInput').value
+    let endpoint = "https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=705303948810324980db093f63a51039&units=metric"
     fetch(endpoint).then((response) => {
     response.json().then(data => {
+        
+        city.innerHTML =`${data.name}`
+          description.innerHTML =`${data.weather[0].description}`
+          humidity.innerHTML =`${data.main.humidity}%`
+          wind.innerHTML =`${data.wind.speed}m/s`
+          minTemp.innerHTML =`${data.main.temp_min}℃`
+          maxTemp.innerHTML =`${data.main.temp_max}℃`
+          pressure.innerHTML =`${data.main.pressure}hPa`
+          feels.innerHTML =`${data.main.feels_like}℃`
+          temp.innerHTML =`${Math.round(data.main.temp)}℃`
+          myCity.innerHTML=`${data.name}, ${data.sys.country}`
+          description2.innerHTML =`${data.weather[0].description}`
+
           
+          console.log(data.name);
     } )
   })
 
